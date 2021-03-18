839ef82a76a24bce83e6d43fd01b4b59d16c2824
Mike Dacey <michael.dacey@uwtsd.ac.uk>
2021-03-18 13:45:52 +0000

    Adder class and unit tests

--- XunitTestProject/UnitTest1.cs
+++ XunitTestProject/UnitTest1.cs
@@ -1,14 +1,37 @@
 using System;
 using Xunit;
+using CodeExampleProject;
 
 namespace XunitTestProject
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void AdderInitTest()
         {
-
+			int expectedValue 0;
+			Adder adder = new Adder();
+			Assert.Equal(expectedValue, adder.Total);
+        }
+		
+		[Fact]
+        public void AdderAddTest()
+        {
+			int expectedValue 5;
+			Adder adder = new Adder();
+			adder.Add(3);
+			adder.Add(2);
+			Assert.Equal(expectedValue, adder.Total);
+        }
+		
+		[Fact]
+        public void AdderResetTest()
+        {
+			int expectedValue 0;
+			Adder adder = new Adder();
+			adder.Add(4);
+			adder.Reset();
+			Assert.Equal(expectedValue, adder.Total);
         }
     }
 }
