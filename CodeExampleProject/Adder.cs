e063a5271e2f4885d64f3f31a9b3c7412334aba4
Mike Dacey <michael.dacey@uwtsd.ac.uk>
2021-03-18 13:52:54 +0000

    Adder class and unit tests3

---
+++ CodeExampleProject/Adder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CodeExampleProject
+{
+    public class Adder
+    {
+        // Attributes
+		private int _sum;
+		
+		// Constructors
+		public Adder()
+		{
+			_sum = 0;
+		}
+		
+		// Properties
+		public int Total
+		{ 
+			get
+			{
+				return _sum;
+			}
+		}
+		
+		// Methods
+		public void Add(int value)
+		{
+			_sum += value;
+		}
+		
+		public void Reset()
+		{
+			_sum = 0;
+		}
+    }
+}
